e995ab6bf32ff755c12734767e1eb80c5df0b680
mira177 <44624770+mira177@users.noreply.github.com>
2019-02-02 17:48:44 -0500

    Update run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-export TOKEN="<token>"
-export MOD_ROLE_ID="<id>"
-export CAN_BAN_ROLE_ID="<id>"
-export CAN_KICK_ROLE_ID="<id>"
-export WARNING_ROLE_ID="<id>"
-export MUTE_ROLE_ID="<id>"
-export LOG_CHANNEL_ID="<id>"
-export SERVER_ID="<id>"
-export OWNER_ID="<id>"
+export TOKEN="<NTM0MDY0NTg4OTM4MDE4ODI2.Dzei1A.i0tAsK2dP0esJmQOiWl2BjrR4II>"
+export MOD_ROLE_ID="<485746137215533067>"
+export CAN_BAN_ROLE_ID="<485746137215533067>"
+export CAN_KICK_ROLE_ID="<485746137215533067>"
+export WARNING_ROLE_ID="<417395427080536074>"
+export MUTE_ROLE_ID="<503960249301204993>"
+export LOG_CHANNEL_ID="<541374623460229195>"
+export SERVER_ID="<485744773324603392>"
+export OWNER_ID="<232911726603927552>"
 export RESTART_COMMAND="sh run_local.sh"
 
 LATEST_BUILD="$(find ./build/libs | sort --version-sort --field-separator=- --key=2,2 | tail -n 1)"
